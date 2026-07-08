c42c3cc11196b008625cf6ef71d095e75a1face4
Qiao Han <sweatybridge@gmail.com>
2026-07-08 23:49:39 +0800

    Upgrade to expo sdk 54

--- dashboard/mobile/babel.config.js
+++ dashboard/mobile/babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    // reanimated/plugin MUST be last.
-    plugins: ["react-native-reanimated/plugin"],
+    // Reanimated v4's babel plugin is added automatically by babel-preset-expo;
+    // the manual "react-native-reanimated/plugin" entry is no longer needed.
   };
 };
