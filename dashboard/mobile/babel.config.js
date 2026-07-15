4badf4ca20fdb61c9cbb1336c3346d40abbd8342
Qiao Han <sweatybridge@gmail.com>
2026-07-16 01:11:53 +0800

    Remove expo app

--- dashboard/mobile/babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo"],
-    // Reanimated v4's babel plugin is added automatically by babel-preset-expo;
-    // the manual "react-native-reanimated/plugin" entry is no longer needed.
-  };
-};
